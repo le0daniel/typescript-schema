17a17eb978a2faadcffcca5c09a5d35d0222e19c
Leo Studer <leodaniel@nitrosoft.ch>
2024-07-17 16:15:38 +0200

    Fixed Schema

--- src/Schema.php
+++ src/Schema.php
@@ -5,19 +5,25 @@ namespace TypescriptSchema;
 use Closure;
 use TypescriptSchema\Contracts\Type;
 use TypescriptSchema\Definition\Complex\ArrayType;
+use TypescriptSchema\Definition\Complex\DiscriminatedUnionType;
 use TypescriptSchema\Definition\Complex\ObjectType;
 use TypescriptSchema\Definition\Complex\RecordType;
 use TypescriptSchema\Definition\Complex\TupleType;
 use TypescriptSchema\Definition\Complex\UnionType;
 use TypescriptSchema\Definition\Primitives\AnyType;
 use TypescriptSchema\Definition\Primitives\BoolType;
+use TypescriptSchema\Definition\Primitives\DateTimeType;
+use TypescriptSchema\Definition\Primitives\EnumType;
 use TypescriptSchema\Definition\Primitives\IntType;
 use TypescriptSchema\Definition\Primitives\LiteralType;
 use TypescriptSchema\Definition\Primitives\StringType;
 use TypescriptSchema\Definition\Primitives\UnknownType;
 use UnitEnum;
 
-final class Schema
+/**
+ * Inherit form this class to create your own shortcuts. All types are immutable, so you can reuse them if needed without any issues.
+ */
+class Schema
 {
     public static function any(): AnyType
     {
@@ -34,34 +40,14 @@ final class Schema
         return StringType::make();
     }
 
-    public static function union(Type... $types): UnionType
-    {
-        return UnionType::make(...$types);
-    }
-
-    public static function literal(string|int|float|bool|UnitEnum $value): LiteralType
-    {
-        return LiteralType::make($value);
-    }
-
-    public static function listOf(Type $type): ArrayType
-    {
-        return new ArrayType($type);
-    }
-
-    public static function array(Type $type): ArrayType
-    {
-        return new ArrayType($type);
-    }
-
-    public static function collection(Type $type): ArrayType
-    {
-        return new ArrayType($type);
-    }
-
     public static function int(): IntType
     {
         return IntType::make();
+    }
+
+    public static function dateTime(?string $format = null): DateTimeType
+    {
+        return DateTimeType::make($format);
     }
 
     public static function bool(): BoolType
@@ -69,7 +55,27 @@ final class Schema
         return BoolType::make();
     }
 
-    public static function dict(Type $ofType): RecordType
+    public static function literal(string|int|float|bool|UnitEnum $value): LiteralType
+    {
+        return LiteralType::make($value);
+    }
+
+    /**
+     * @template T of UnitEnum
+     * @param class-string<T> $className
+     * @return EnumType
+     */
+    public static function enum(string $className): EnumType
+    {
+        return EnumType::make($className);
+    }
+
+    public static function array(Type $type): ArrayType
+    {
+        return new ArrayType($type);
+    }
+
+    public static function record(Type $ofType): RecordType
     {
         return new RecordType($ofType);
     }
@@ -82,6 +88,16 @@ final class Schema
     public static function tuple(Type ... $types): TupleType
     {
         return TupleType::make(...$types);
+    }
+
+    public static function union(Type... $types): UnionType
+    {
+        return UnionType::make(...$types);
+    }
+
+    public static function discriminatedUnion(string $discriminator, Type ... $types): DiscriminatedUnionType
+    {
+        return DiscriminatedUnionType::make($discriminator, ...$types);
     }
 
     public static function literalUnion(array $literals): UnionType
