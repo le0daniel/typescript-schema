d695637d39132bd39ec984a33dffc22f45333510
Leo Studer <leodaniel@nitrosoft.ch>
2024-07-22 12:51:00 +0200

    Default translations for de, en, fr

---
+++ src/Locales/fr.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+return [
+    'failed' => 'Données non valides',
+    'coercion_failure' => 'La valeur doit être du type :expected',
+    'invalid_type' => 'La valeur doit être du type :expected',
+    'invalid_key' => 'La valeur est obligatoire',
+    'generic_failure' => 'Valeur non valide',
+    'internal_error' => 'Valeur non valide',
+    'string.invalid_regex' => 'La valeur ne correspond pas au modèle attendu',
+    'string.invalid_email' => 'La valeur n\'est pas une adresse e-mail valide',
+    'string.invalid_min' => 'La valeur doit comporter au moins :min caractères',
+    'string.invalid_empty' => 'La valeur ne peut pas être vide',
+    'string.invalid_alphanumeric' => 'La valeur doit être alphanumérique',
+    'string.invalid_max' => 'La valeur doit être inférieure à :max caractères',
+    'string.invalid_ends_with' => 'La valeur doit se terminer par :endsWith',
+    'string.invalid_starts_with' => 'La valeur doit commencer par :startsWith',
+    'int.invalid_min' => 'La valeur doit être au moins :min',
+    'int.invalid_max' => 'La valeur doit être inférieure à :max',
+] ;
