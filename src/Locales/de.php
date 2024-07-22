3b8978b3912992cdc7995df8c0e3aff6c3d45a28
Leo Studer <leodaniel@nitrosoft.ch>
2024-07-22 12:30:31 +0200

    Removed JsonSerialize from Issue

---
+++ src/Locales/de.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+return [
+    'failed' => 'Ungültige Daten',
+    'coercion_failure' => 'Wert muss vom Typ :expected sein',
+    'invalid_type' => 'Wert muss vom Typ :expected sein',
+    'invalid_key' => 'Wert ist erforderlich',
+    'generic_failure' => 'Ungültiger Wert',
+    'internal_error' => 'Ungültiger Wert',
+];
---
+++ src/Locales/en.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+return [
+    'failed' => 'Invalid data',
+    'coercion_failure' => 'Value needs to be of type :expected',
+    'invalid_type' => 'Value needs to be of type :expected',
+    'invalid_key' => 'Value is required',
+    'generic_failure' => 'Invalid value',
+    'internal_error' => 'Invalid value',
+];
