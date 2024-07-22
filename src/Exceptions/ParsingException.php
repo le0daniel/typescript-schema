accaf17d2bd0872d2e673d9e93e487d204554077
Leo Studer <leodaniel@nitrosoft.ch>
2024-07-22 12:35:23 +0200

    Removed JsonSerialize from Issue

--- src/Exceptions/ParsingException.php
+++ src/Exceptions/ParsingException.php
@@ -3,14 +3,61 @@
 namespace TypescriptSchema\Exceptions;
 
 use Exception;
+use JsonSerializable;
+use TypescriptSchema\Contracts\Localizer;
 
-class ParsingException extends Exception
+class ParsingException extends Exception implements JsonSerializable
 {
+    private const string DEFAULT_LOCALE = 'en';
 
+    private ?Localizer $localizer = null;
+
+    private string $locale = self::DEFAULT_LOCALE;
+
+    /**
+     * @param array<Issue> $issues
+     */
     public function __construct(
         public readonly array $issues
     )
     {
         parent::__construct('Failed parsing the schema');
+    }
+
+    private function getLocalizer(): Localizer
+    {
+        return $this->localizer ??= new \TypescriptSchema\Helpers\Localizer();
+    }
+
+    public function setLocalizer(Localizer $localizer): self
+    {
+        $this->localizer = $localizer;
+        return $this;
+    }
+
+    public function setLocale(string $locale): self
+    {
+        $this->locale = $locale;
+        return $this;
+    }
+
+    public function toArray(): array
+    {
+        $groupedIssues = [];
+        foreach ($this->issues as $issue) {
+            $groupedIssues[$issue->pathAsString()][] = $this->getLocalizer()->localize(
+                $this->locale, $issue->getLocalizationKey(), $issue->metadata,
+            );
+        }
+
+        return [
+            'message' => $this->getLocalizer()->localize($this->locale, 'failed'),
+            'issues' => $groupedIssues,
+        ];
+    }
+
+    public function jsonSerialize(): array
+    {
+        return $this->toArray();
     }
 }
---
+++ tests/Exceptions/ParsingExceptionTest.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+namespace TypescriptSchema\Tests\Exceptions;
+
+use TypescriptSchema\Exceptions\Issue;
+use TypescriptSchema\Exceptions\ParsingException;
+use PHPUnit\Framework\TestCase;
+
+class ParsingExceptionTest extends TestCase
+{
+
+    public function testSerialization(): void
+    {
+        $exception = new ParsingException([
+            Issue::invalidType('string', 'array', ['name']),
+            Issue::coercionFailure('string', 'array', ['name']),
+        ]);
+
+        self::assertEquals([
+            'message' => 'Invalid data',
+            'issues' => [
+                'name' => [
+                    'Value needs to be of type string',
+                    'Value needs to be of type string',
+                ]
+            ],
+        ], $exception->jsonSerialize());
+
+        self::assertEquals([
+            'message' => 'Ungültige Daten',
+            'issues' => [
+                'name' => [
+                    'Wert muss vom Typ string sein',
+                    'Wert muss vom Typ string sein',
+                ]
+            ],
+        ], $exception->setLocale('de')->jsonSerialize());
+    }
+
+}
